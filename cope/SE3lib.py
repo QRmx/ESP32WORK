d5cd0d7ebe72817b9c90795dcad639b187c36148
QRmx <105599390+QRmx@users.noreply.github.com>
2020-02-17 13:05:17 +0000

    Fix #131

--- cope/SE3lib.py
+++ cope/SE3lib.py
@@ -356,4 +356,28 @@ def VecToJac(vec):
       snr = np.sin(nr)
       JSO3 = (snr/nr)*np.eye(3) + (1-snr/nr)*axis[np.newaxis]*axis[np.newaxis].T + ((1-cnr)/nr)*Hat(axis)
     return JSO3
-  elif vec.shape[0] == 6: # Jacobian 
+  elif vec.shape[0] == 6: # Jacobian of SE3
+    rho = vec[:3]
+    phi = vec[3:]
+    nr = np.linalg.norm(phi)
+    if nr < tiny:
+      #If the angle is small, fall back on the series representation
+      JSE3 = VecToJacSeries(phi,10);
+    else:
+      JSO3 = VecToJac(phi)
+      Q = VecToQ(vec)
+      JSE3 = np.zeros((6,6))
+      JSE3[:3,:3] = JSO3
+      JSE3[:3,3:] = Q
+      JSE3[3:,3:] = JSO3
+    return JSE3
+  else:
+    raise ValueError("Invalid input vector length\n")
+
+
+def VecToJacSeries(vec,N):
+  """ 
+  Construction of the J matrix from Taylor series
+  @param vec: a 3x1 vector for SO3 or a 6x1 vector for SE3 (input)
+  @param N:   number of terms to include in the series (input)
+  @param J:   a 3x3 J matrix for SO3 or a 6x6 J matrix for SE3 (ou
