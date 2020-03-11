9c13a7c0b93848721969a5ad4d35cc0df1ef6a11
QRmx <105599390+QRmx@users.noreply.github.com>
2020-03-11 12:45:16 +0000

    power: increment wakeup_count when save_wakeup_count failed.

--- cope/particlelib.py
+++ cope/particlelib.py
@@ -18,4 +18,31 @@
 # You should have received a copy of the GNU General Public License along with
 # python-cope. If not, see <http://www.gnu.org/licenses/>.
 
-import numpy  a
+import numpy  as np
+import trimesh
+import scipy as sp
+from scipy.stats import norm
+import cope.SE3lib as SE3
+import bisect
+import cope.transformation as tr
+import random
+import time
+import copy
+import matplotlib.pyplot as plt
+
+
+class Region(object):
+  def __init__(self, particles, delta_rot,delta_trans):
+    self.particles = particles #List of particles (transformations)
+    self.delta_rot = delta_rot
+    self.delta_trans = delta_trans
+
+def IsInside(point,center,radius):
+  if np.linalg.norm(point-center) < radius:
+    return True
+  return False
+
+
+def EvenDensityCover(region, M):
+  '''Input: Region V_n - sampling region represented as a union of neighborhoods, M - number of particles to sample per neighborhood
+  Output: a set o
