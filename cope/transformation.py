469d51f19da859f94671ae7b6dc3497a77a92f79
QRmx <105599390+QRmx@users.noreply.github.com>
2020-04-22 17:23:41 +0000

    Fixes #25

--- cope/transformation.py
+++ cope/transformation.py
@@ -84,4 +84,19 @@ Angles are in radians unless specified otherwise.
 Quaternions w+ix+jy+kz are represented as [w, x, y, z].
 
 A triple of Euler angles can be applied/interpreted in 24 ways, which can
-be specified usi
+be specified using a 4 character string or encoded 4-tuple:
+
+  *Axes 4-string*: e.g. 'sxyz' or 'ryxy'
+
+  - first character : rotations are applied to 's'tatic or 'r'otating frame
+  - remaining characters : successive rotation axis 'x', 'y', or 'z'
+
+  *Axes 4-tuple*: e.g. (0, 0, 0, 0) or (1, 1, 1, 1)
+
+  - inner axis: code of axis ('x':0, 'y':1, 'z':2) of rightmost matrix.
+  - parity : even (0) if inner axis 'x' is followed by 'y', 'y' is followed
+    by 'z', or 'z' is followed by 'x'. Otherwise odd (1).
+  - repetition : first and last axis are same (1) or different (0).
+  - frame : rotations are applied to static (0) or rotating (1) frame.
+
+Other Python packages and modules for 3D transformations and quaternio
