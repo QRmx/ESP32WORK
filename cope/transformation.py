d351c2c794e2ad1c8e113b0c041dfe634c1a4b50
QRmx <105599390+QRmx@users.noreply.github.com>
2020-05-04 14:46:29 +0000

    ACPI: export symbol acpi_get_table_with_size

--- cope/transformation.py
+++ cope/transformation.py
@@ -209,4 +209,38 @@ def identity_matrix():
     >>> numpy.allclose(I, numpy.dot(I, I))
     True
     >>> numpy.sum(I), numpy.trace(I)
-   
+    (4.0, 4.0)
+    >>> numpy.allclose(I, numpy.identity(4))
+    True
+
+    """
+    return numpy.identity(4)
+
+
+def translation_matrix(direction):
+    """Return matrix to translate by direction vector.
+
+    >>> v = numpy.random.random(3) - 0.5
+    >>> numpy.allclose(v, translation_matrix(v)[:3, 3])
+    True
+
+    """
+    M = numpy.identity(4)
+    M[:3, 3] = direction[:3]
+    return M
+
+
+def translation_from_matrix(matrix):
+    """Return translation vector from translation matrix.
+
+    >>> v0 = numpy.random.random(3) - 0.5
+    >>> v1 = translation_from_matrix(translation_matrix(v0))
+    >>> numpy.allclose(v0, v1)
+    True
+
+    """
+    return numpy.array(matrix, copy=False)[:3, 3].copy()
+
+
+def reflection_matrix(point, normal):
+    """Ret
