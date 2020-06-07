9fe3671cc8531ff2323238a59a06214ca45303d5
QRmx <105599390+QRmx@users.noreply.github.com>
2020-06-07 13:00:53 +0000

    Base version

--- cope/transformation.py
+++ cope/transformation.py
@@ -853,4 +853,29 @@ def compose_matrix(scale=None, shear=None, angles=None, translate=None,
         S[1, 1] = scale[1]
         S[2, 2] = scale[2]
         M = numpy.dot(M, S)
-    M /= M[3, 
+    M /= M[3, 3]
+    return M
+
+
+def orthogonalization_matrix(lengths, angles):
+    """Return orthogonalization matrix for crystallographic cell coordinates.
+
+    Angles are expected in degrees.
+
+    The de-orthogonalization matrix is the inverse.
+
+    >>> O = orthogonalization_matrix([10, 10, 10], [90, 90, 90])
+    >>> numpy.allclose(O[:3, :3], numpy.identity(3, float) * 10)
+    True
+    >>> O = orthogonalization_matrix([9.8, 12.0, 15.5], [87.2, 80.7, 69.7])
+    >>> numpy.allclose(numpy.sum(O), 43.063229)
+    True
+
+    """
+    a, b, c = lengths
+    angles = numpy.radians(angles)
+    sina, sinb, _ = numpy.sin(angles)
+    cosa, cosb, cosg = numpy.cos(angles)
+    co = (cosa * cosb - cosg) / (sina * sinb)
+    return numpy.array([
+   
