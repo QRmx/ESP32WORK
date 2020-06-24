b3169533a9fa8b936fc19b0a7ad404eb7c0323a1
QRmx <105599390+QRmx@users.noreply.github.com>
2020-06-24 13:51:47 +0000

    bump version, update changelog

--- cope/transformation.py
+++ cope/transformation.py
@@ -1307,4 +1307,27 @@ def quaternion_from_matrix(matrix, isprecise=False):
     >>> is_same_transform(R, quaternion_matrix(q))
     True
     >>> R = euler_matrix(0.0, 0.0, numpy.pi/2.0)
-   
+    >>> numpy.allclose(quaternion_from_matrix(R, isprecise=False),
+    ...                quaternion_from_matrix(R, isprecise=True))
+    True
+
+    """
+    M = numpy.array(matrix, dtype=numpy.float64, copy=False)[:4, :4]
+    if isprecise:
+        q = numpy.empty((4, ))
+        t = numpy.trace(M)
+        if t > M[3, 3]:
+            q[0] = t
+            q[3] = M[1, 0] - M[0, 1]
+            q[2] = M[0, 2] - M[2, 0]
+            q[1] = M[2, 1] - M[1, 2]
+        else:
+            i, j, k = 1, 2, 3
+            if M[1, 1] > M[0, 0]:
+                i, j, k = 2, 3, 1
+            if M[2, 2] > M[i, i]:
+                i, j, k = 3, 1, 2
+            t = M[i, i] - (M[j, j] + M[k, k]) + M[3, 3]
+            q[i] = t
+            q[j] = M[i, j] + M[j, i]
+            q[k] = M
