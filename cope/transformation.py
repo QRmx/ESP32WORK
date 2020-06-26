fb6991fbe6ffbfb9968e4c37747cebb83235ebb9
QRmx <105599390+QRmx@users.noreply.github.com>
2020-06-26 22:02:14 +0000

    Changed a comment

--- cope/transformation.py
+++ cope/transformation.py
@@ -1408,4 +1408,30 @@ def quaternion_real(quaternion):
     3.0
 
     """
-    return float(quate
+    return float(quaternion[0])
+
+
+def quaternion_imag(quaternion):
+    """Return imaginary part of quaternion.
+
+    >>> quaternion_imag([3, 0, 1, 2])
+    array([ 0.,  1.,  2.])
+
+    """
+    return numpy.array(quaternion[1:4], dtype=numpy.float64, copy=True)
+
+
+def quaternion_slerp(quat0, quat1, fraction, spin=0, shortestpath=True):
+    """Return spherical linear interpolation between two quaternions.
+
+    >>> q0 = random_quaternion()
+    >>> q1 = random_quaternion()
+    >>> q = quaternion_slerp(q0, q1, 0)
+    >>> numpy.allclose(q, q0)
+    True
+    >>> q = quaternion_slerp(q0, q1, 1, 1)
+    >>> numpy.allclose(q, q1)
+    True
+    >>> q = quaternion_slerp(q0, q1, 0.5)
+    >>> angle = math.acos(numpy.dot(q0, q))
+    >>> numpy.
