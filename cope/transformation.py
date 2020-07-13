1e68a6445ab5a722d5d1ee815b2670151653dc95
QRmx <105599390+QRmx@users.noreply.github.com>
2020-07-13 14:42:26 +0000

    Minor format fix

--- cope/transformation.py
+++ cope/transformation.py
@@ -1686,4 +1686,30 @@ def vector_norm(data, axis=None, out=None):
     >>> numpy.allclose(n, numpy.linalg.norm(v))
     True
     >>> v = numpy.random.rand(6, 5, 3)
-    >>> n = vector_norm(v, 
+    >>> n = vector_norm(v, axis=-1)
+    >>> numpy.allclose(n, numpy.sqrt(numpy.sum(v*v, axis=2)))
+    True
+    >>> n = vector_norm(v, axis=1)
+    >>> numpy.allclose(n, numpy.sqrt(numpy.sum(v*v, axis=1)))
+    True
+    >>> v = numpy.random.rand(5, 4, 3)
+    >>> n = numpy.empty((5, 3))
+    >>> vector_norm(v, axis=1, out=n)
+    >>> numpy.allclose(n, numpy.sqrt(numpy.sum(v*v, axis=1)))
+    True
+    >>> vector_norm([])
+    0.0
+    >>> vector_norm([1])
+    1.0
+
+    """
+    data = numpy.array(data, dtype=numpy.float64, copy=True)
+    if out is None:
+        if data.ndim == 1:
+            return math.sqrt(numpy.dot(data, data))
+        data *= data
+        out = numpy.atleast_1d(numpy.sum(data, axis=axis))
+        numpy.sqrt(out, out)
+        return out
+    else:
+        
