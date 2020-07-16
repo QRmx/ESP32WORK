ef9492aa62a0c68858f1aa281240f2de7bc2b0a3
QRmx <105599390+QRmx@users.noreply.github.com>
2020-07-16 16:14:11 +0000

    fix package

--- cope/transformation.py
+++ cope/transformation.py
@@ -1733,4 +1733,33 @@ def unit_vector(data, axis=None, out=None):
     >>> v2 = v0 / numpy.expand_dims(numpy.sqrt(numpy.sum(v0*v0, axis=1)), 1)
     >>> numpy.allclose(v1, v2)
     True
-    >>> v1 = numpy.empty((5, 4, 
+    >>> v1 = numpy.empty((5, 4, 3))
+    >>> unit_vector(v0, axis=1, out=v1)
+    >>> numpy.allclose(v1, v2)
+    True
+    >>> list(unit_vector([]))
+    []
+    >>> list(unit_vector([1]))
+    [1.0]
+
+    """
+    if out is None:
+        data = numpy.array(data, dtype=numpy.float64, copy=True)
+        if data.ndim == 1:
+            data /= math.sqrt(numpy.dot(data, data))
+            return data
+    else:
+        if out is not data:
+            out[:] = numpy.array(data, copy=False)
+        data = out
+    length = numpy.atleast_1d(numpy.sum(data*data, axis))
+    numpy.sqrt(length, length)
+    if axis is not None:
+        length = numpy.expand_dims(length, axis)
+    data /= length
+    if out is None:
+        return data
+
+
+def random_vector(size):
+    """Return 
