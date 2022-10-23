274a9a5b95f24a21692ae339aed24408bb97a7a6
QRmx <105599390+QRmx@users.noreply.github.com>
2022-10-23 05:52:07 +0000

    Fix checkstyle issues.

---
+++ examples/test_axxb_covariance.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+import copy
+import math
+import numpy as np
+import random
+import pickle
+import cope.SE3lib as SE3
+import cope.axxbcovariance as axxb
+import matplotlib.pyplot as plt
+
+# Read data files
+filename = "data/pattern_tfs"
+pattern_tfs =  pickle.load(open( filename, "rb" ) )
+filename = "data/robot_tfs"
+robot_tfs =  pickle.load(open( filename, "rb" ) )
+
+sigmaA = 1e
