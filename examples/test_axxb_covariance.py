e10bc975ef21ad5d50a947d2f030d866c1eef8a3
QRmx <105599390+QRmx@users.noreply.github.com>
2022-11-03 13:23:08 +0000

    update List

--- examples/test_axxb_covariance.py
+++ examples/test_axxb_covariance.py
@@ -34,4 +34,20 @@ txlist = []
 sigmatx_list = []
 for n in range(iters):
     alpha = []
-    beta =
+    beta = []
+    ta = []
+    tb = []
+    # Generate data-A and B matrices
+    for i in range(ksamples):
+        # note this
+        rand_number_1 = int(np.random.uniform(0,datasize))
+        rand_number_2 = int(np.random.uniform(0,datasize))
+        while rand_number_1==rand_number_2:
+            rand_number_2 = int(np.random.uniform(0,datasize))
+        A = np.dot(robot_tfs[rand_number_1],np.linalg.inv(robot_tfs[rand_number_2]))
+        B = np.dot(pattern_tfs[rand_number_1],np.linalg.inv(pattern_tfs[rand_number_2]))
+        alpha.append(SE3.RotToVec(A[:3,:3]))
+        beta.append(SE3.RotToVec(B[:3,:3]))
+        ta.append(A[:3,3])
+        tb.append(B[:3,3])
+    Rxinit,tx
