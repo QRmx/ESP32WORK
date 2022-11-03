8fde1996a42661c4e8dfad72177bf57c85533a9a
QRmx <105599390+QRmx@users.noreply.github.com>
2022-11-03 06:19:10 +0000

    pms

--- examples/test_axxb_covariance.py
+++ examples/test_axxb_covariance.py
@@ -14,4 +14,24 @@ pattern_tfs =  pickle.load(open( filename, "rb" ) )
 filename = "data/robot_tfs"
 robot_tfs =  pickle.load(open( filename, "rb" ) )
 
-sigmaA = 1e
+sigmaA = 1e-10*np.diag((1, 1, 1, 1, 1, 1))
+sigmaRa = sigmaA[3:,3:]
+sigmata = sigmaA[:3,:3]
+
+sigmaRb = np.array([[  4.15625435e-05,  -2.88693145e-05,  -6.06526440e-06],
+                    [ -2.88693145e-05,   3.20952008e-04,  -1.44817304e-06],
+                    [ -6.06526440e-06,  -1.44817304e-06,   1.43937081e-05]])
+sigmatb = np.array([[  1.95293655e-04,   2.12627214e-05,  -1.06674886e-05],
+                    [  2.12627214e-05,   4.44314426e-05,   3.86787591e-06],
+                    [ -1.06674886e-05,   3.86787591e-06,   2.13069579e-05]])
+
+datasize = len(pattern_tfs)
+ksamples = 30
+iters = 500
+Rxlist = []
+sigmaRx_list = []
+txlist = []
+sigmatx_list = []
+for n in range(iters):
+    alpha = []
+    beta =
