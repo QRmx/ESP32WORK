896884af51a05830b85e29bfc6475255f8a67ad7
QRmx <105599390+QRmx@users.noreply.github.com>
2022-12-20 18:23:03 +0000

    Some code cleaning

--- examples/test_cov_inverse_tran.py
+++ examples/test_cov_inverse_tran.py
@@ -12,3 +12,14 @@ sigma = scale*np.diag((0.1,0.2,0.5,0.5,0.2,0.3))
 
 vec_Tinv = cope.TranToVec(np.linalg.inv(T))
 
+xi_vec_Tinv = []
+for i in range(ksamples):
+    xisample = np.random.multivariate_normal(np.zeros(6),sigma)
+    Tsample = np.dot(cope.VecToTran(xisample),T)
+    Tinvsample = np.linalg.inv(Tsample)
+    xi_vec_Tinv.append(cope.TranToVec(np.dot(Tinvsample,T)))
+    
+print np.cov(np.transpose(xi_vec_Tinv))[:3,:3]
+print cope.CovInverseTran(T,sigma)[1][:3,:3]
+print cope.CovInverseTran(T,sigma)[1][3:,3:]
+print np.cov(np.transpose(xi_vec_Tinv))[3:,3:]
---
+++ examples/test_cov_inverse_tran_separate.py
@@ -0,0 +1,14 @@
+import numpy as np
+import cope
+import cope.transformation as tr
+
+
+ksamples = 500
+T =  tr.random_rotation_matrix()
+T[:3,3] = tr.random_vector(3)
+R = T[:3,:3]
+Rinv = np.linalg.inv(R)
+t = T[:3,3]
+tinv = -np.dot(Rinv,t)
+scale = 1e-3
+sigmat = scale*n
