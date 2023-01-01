a742687a27575cb91198dd130fa1893b2d8767f5
QRmx <105599390+QRmx@users.noreply.github.com>
2023-01-01 19:20:08 +0000

    removed unused codes

--- examples/test_cov_inverse_tran_separate.py
+++ examples/test_cov_inverse_tran_separate.py
@@ -11,4 +11,24 @@ Rinv = np.linalg.inv(R)
 t = T[:3,3]
 tinv = -np.dot(Rinv,t)
 scale = 1e-3
-sigmat = scale*n
+sigmat = scale*np.diag(tr.random_vector(3))#(0.1,0.2,0.5))
+sigmaR = scale*np.diag(tr.random_vector(3))#(0.5,0.2,0.3))
+
+xi_vec_Rinv = []
+xi_tinv = []
+
+for i in range(ksamples):
+    xisampleR = np.random.multivariate_normal(np.zeros(3),sigmaR)
+    xisamplet = np.random.multivariate_normal(np.zeros(3),sigmat)
+    Rsample = np.dot(cope.VecToRot(xisampleR),R)
+    Rinvsample = np.linalg.inv(Rsample)
+    tsample = t + xisamplet
+    tinvsample = -np.dot(Rinvsample,tsample)
+    xi_vec_Rinv.append(cope.RotToVec(np.dot(Rsample,Rinv)))
+    xi_tinv.append(tinvsample - tinv)
+res = cope.CovInverseTranWithSeparateRotTrans(R,sigmaR,t,sigmat)
+print np.cov(np.transpose(xi_vec_Rinv))
+print res[1]
+print np.cov(np.transpose(xi_tinv))
+print res[3]
+
---
+++ examples/test_estimation_clutter_env.py
@@ -0,0 +1,4 @@
+import trimesh
+import numpy as np
+import cope.SE3lib as SE3
+im
