86064f46cbdb840651a4b39cf7ae9426ee60a671
QRmx <105599390+QRmx@users.noreply.github.com>
2023-01-05 05:26:30 +0000

    drm/i915: only enable sdvo hotplug irq if needed

--- examples/test_estimation_clutter_env.py
+++ examples/test_estimation_clutter_env.py
@@ -1,4 +1,33 @@
 import trimesh
 import numpy as np
 import cope.SE3lib as SE3
-im
+import cope.particlelib as ptcl
+import cope.transformation as tr
+import copy
+import time
+import pickle
+
+
+extents    = [0.13,0.1,0.3]
+mesh       = trimesh.creation.box(extents)
+pkl_file   = open('data/woodstick_w_dict.p', 'rb')
+angle_dict = pickle.load(pkl_file)
+pkl_file.close()
+
+# ext = [0.05,0.05,0.34]
+# other_box = trimesh.creation.box(ext)
+# other_box.apply_translation([0.1,0.01,.02])
+
+# rack = trimesh.load_mesh('data/Rack1.ply')
+# rack.apply_translation([-0.163,-0.01,-0.17])
+# rack.apply_transform(tr.euler_matrix(-3.14/5.,0,3.14/6.))
+# rack.apply_translation([0.075,-0.075,0])
+
+# clutter = copy.deepcopy(other_box + rack)
+
+
+pos_err = 2e-3
+nor_err = 5./180.0*np.pi
+
+# Uncertainty & params
+sigma0 = np.diag([0.0
